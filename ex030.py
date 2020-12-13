679d2f100476a5ade61f7a23b94962c7c5959700
HongMyunggi <ghdaudrl8888@naver.com>
2020-12-14 01:42:46 +0900

    addedfile

---
+++ ex030.py
@@ -0,0 +1,6 @@
+#30
+string = 'abcd'
+string.replace('b', 'B') # string은 변경되지않았다
+print(string)
+# abcd가 나온다
+# 만약 b를 B로 바꾸고싶다면 a = string.replace('b','B')를 하면 된다.
---
+++ ex040.py
@@ -0,0 +1,7 @@
+#40
+data = "   삼성전자   " #data에 대해서 strip함수를 적용시켰다.
+a = data.strip() # strip()은 특정문자를 제거할수있다.
+#strip()에 공백을 두면서 좌우의 공백을 제거했다. 
+#a = data.strip()을 하면서 a에 strip된 값을 대입했다.
+print(a)
+print(data)# print(data.strip())과 비교해보기위해서 사용하였다.
